6ae9cb3ee4b2a291e161d572792c107a48f5c849
Tonni Ramdani <tonniramdani@gmail.com>
2025-01-14 16:05:51 +0700

    menambahkan fitur cari pada halaman peminjaman

--- peminjaman.php
+++ peminjaman.php
@@ -1,9 +1,21 @@
 <h1 class="mt-4">Pinjam Buku</h1>
 <div class="row">
     <div class="col-md-12">
-        <a href="?page=peminjaman_tambah" class="btn btn-primary"><i class="fa fa-plus"></i>Tambah Peminjaman</a>
-        <br>
-        <br>
+        <div class="d-flex justify-content-between align-items-center mb-3">
+            <a href="?page=peminjaman_tambah" class="btn btn-primary"><i class="fa fa-plus"></i>Tambah Peminjaman</a>
+            <?php 
+                if($_SESSION['user']['level'] == 'admin'){
+            ?>
+            <form method="GET" action="" class="d-flex align-items-center">
+                <input type="hidden" name="page" value="peminjaman">
+                <input type="text" name="cari" class="form-control form-control-sm" placeholder="Cari Nama" style="width: 200px;">
+                <button type="submit" class="btn btn-secondary btn-sm ms-2">Cari</button>
+            </form>
+            <?php
+                } 
+            ?>
+        </div>
+
         <table class="table table-bordered" id="dataTable" width="100%" cellspacin="5">
             <tr>
                 <th>No</th>
@@ -15,12 +27,21 @@
                 <th>Aksi</th>
             </tr>
             <?php
-            $i = 1;
+                $i = 1;
+
                 if($_SESSION['user']['level'] != 'admin'){
                     $query = mysqli_query($koneksi, "SELECT * FROM peminjaman LEFT JOIN user on user.id_user = peminjaman.id_user LEFT JOIN buku on buku.id_buku = peminjaman.id_buku WHERE peminjaman.id_user=" . $_SESSION['user']['id_user']);
-                } else {
-                    $query = mysqli_query($koneksi, "SELECT * FROM peminjaman LEFT JOIN user on user.id_user = peminjaman.id_user LEFT JOIN buku on buku.id_buku = peminjaman.id_buku");
+                } else {                    
+                    $cari = isset($_GET['cari']) ? mysqli_real_escape_string($koneksi, $_GET['cari']) : '';
+    
+                    $sql = "SELECT * FROM peminjaman LEFT JOIN user on user.id_user = peminjaman.id_user LEFT JOIN buku on buku.id_buku = peminjaman.id_buku";
+                    if (!empty($cari)) {
+                        $sql .= " WHERE user.nama LIKE '%$cari%'";
+                    }
+
+                    $query = mysqli_query($koneksi, $sql);
                 }
+
                 while($data = mysqli_fetch_array($query)){
                     ?>
                     <tr>
