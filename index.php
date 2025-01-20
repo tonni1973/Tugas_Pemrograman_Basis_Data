665cadf0310e5b27c3152903054ec9c768ede9df
Tonni Ramdani <tonniramdani@gmail.com>
2025-01-20 08:14:48 +0700

    edit footer

--- index.php
+++ index.php
@@ -117,12 +117,7 @@ if (!isset($_SESSION['user'])) {
             <footer class="py-4 bg-light mt-auto">
                 <div class="container-fluid px-4">
                     <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                        <div>
-                            <a href="#">Privacy Policy</a>
-                            &middot;
-                            <a href="#">Terms &amp; Conditions</a>
-                        </div>
+                        <div class="text-muted">Perpustakaan Digital</div>
                     </div>
                 </div>
             </footer>
